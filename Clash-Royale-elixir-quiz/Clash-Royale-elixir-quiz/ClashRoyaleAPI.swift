2158f6dffe1140024025af75706c5737ac6f6f12
flavio.cheung <flavio.cheung@gmail.com>
2025-10-28 09:24:44 +0100

    use the Clash Royale API and craete the quiz view

---
+++ Clash-Royale-elixir-quiz/Clash-Royale-elixir-quiz/ClashRoyaleAPI.swift
@@ -0,0 +1,8 @@
+//
+//  ClashRoyaleAPI.swift
+//  Clash-Royale-elixir-quiz
+//
+//  Created by Ihub Innopot on 28.10.25.
+//
+
+import Foundation
---
+++ Clash-Royale-elixir-quiz/Clash-Royale-elixir-quiz/CradsResponse.swift
@@ -0,0 +1,8 @@
+//
+//  CradsResponse.swift
+//  Clash-Royale-elixir-quiz
+//
+//  Created by Ihub Innopot on 28.10.25.
+//
+
+import Foundation
